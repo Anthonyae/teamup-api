efec8fccfd1339fd4e5e0d58784c83eac5e474ff
Tony <anthony.ae@outlook.com>
2024-07-23 14:34:51 -0500

    chore: update code quality script

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-black --config .black.toml .
-pylint --rcfile .pylintrc app/.
+EXIT_STATUS=0
+
+black --config .black.toml . || ((EXIT_STATUS++))
+pylint --rcfile .pylintrc app/. || ((EXIT_STATUS++))
+
+exit $EXIT_STATUS
