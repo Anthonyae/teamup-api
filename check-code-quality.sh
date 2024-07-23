805b5c441f3a80db5fdc0533b62ab1f17534c03c
Tony <anthony.ae@outlook.com>
2024-07-23 15:18:57 -0500

    chore: update code quality script

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -5,5 +5,5 @@ EXIT_STATUS=0
 black --config .black.toml . || ((EXIT_STATUS++))
 pylint --rcfile .pylintrc app/. || ((EXIT_STATUS++))
 
-
+echo "Number of apps ran with error codes: $EXIT_STATUS"
 exit $EXIT_STATUS
